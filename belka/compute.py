476e242f47c99fa82f122885a8175e88aea78a27
Ken Pepple <ken@pepple.info>
2013-03-25 01:50:30 -0700

    added default for compute args. pep8 fixes.

--- belka/compute.py
+++ belka/compute.py
@@ -12,23 +12,22 @@ class Compute(Command):
     '''Query compute nodes for statistics'''
 
     log = logging.getLogger(__name__)
-    
-    # show_individual_stats = True
-    # show_aggregate_stats = True
-    # show_header = True
 
     def get_parser(self, prog_name):
         parser = super(Compute, self).get_parser(prog_name)
-        parser.add_argument('--noheader', action='store_true', default = False)
-        parser.add_argument('--noindividual', action='store_true', default = False)
-        parser.add_argument('--noaggregate', action='store_true', default = False)
+        parser.add_argument('--noheader', action='store_true',
+                            default=False)
+        parser.add_argument('--noindividual', action='store_true',
+                            default=False)
+        parser.add_argument('--noaggregate', action='store_true',
+                            default=False)
         return parser
 
-    def _headers(self,token,tenant_id):
+    def _headers(self, token, tenant_id):
         return {"X-Auth-Token": token, "X-Auth-Project-Id": tenant_id,
-            "User-Agent": "belka", "Accept": "application/json"}
+                "User-Agent": "belka", "Accept": "application/json"}
 
-    def _csv_header(self,keys):
+    def _csv_header(self, keys):
         self.app.stdout.write('datetime,')
         self.app.stdout.write(",".join(keys))
         self.app.stdout.write("\n")
@@ -60,40 +59,46 @@ class Compute(Command):
         self.app.stdout.write(str(running_vms) + ",")
         self.app.stdout.write(str(vcpus_used) + ",")
         self.app.stdout.write(str(memory_mb_used) + ",")
-        self.app.stdout.write("All\n")
+        self.app.stdout.write("AllHosts\n")
 
-    def _hypervisor_list(self,tenant_id, compute_endpoint, token, tenant_name):
+    def _hypervisor_list(self, tenant_id, compute_endpoint,
+                         token, tenant_name):
         '''Return a list of hypervisor ids in the compute environment'''
         url = ("%s/os-hypervisors" % compute_endpoint)
-        hdr = self._headers(token,tenant_id)
+        hdr = self._headers(token, tenant_id)
         r = requests.get(url, headers=hdr)
         self.log.debug(r.json())
         hid = [h['id'] for h in r.json()['hypervisors']]
         return hid
 
-    def _hypervisor_detail(self,tenant_id, compute_endpoint, token, tenant_name, hid):
+    def _hypervisor_detail(self, tenant_id, compute_endpoint,
+                           token, tenant_name, hid):
         '''Return dict of a specific hypervisor details'''
-        url = ("%s/os-hypervisors/%s" % (compute_endpoint,str(hid)))
-        hdr = self._headers(token,tenant_id)
+        url = ("%s/os-hypervisors/%s" % (compute_endpoint, str(hid)))
+        hdr = self._headers(token, tenant_id)
         r = requests.get(url, headers=hdr)
         stat = r.json()['hypervisor']
-        return dict(memory_mb_used=stat['memory_mb_used'], memory_mb=stat['memory_mb'],
-            running_vms=stat['running_vms'], vcpus=stat['vcpus'],
-            vcpus_used=stat['vcpus_used'], hypervisor_hostname=stat['hypervisor_hostname'],
-            current_workload=stat['current_workload'])
+        return dict(memory_mb_used=stat['memory_mb_used'],
+                    memory_mb=stat['memory_mb'],
+                    running_vms=stat['running_vms'],
+                    vcpus=stat['vcpus'], vcpus_used=stat['vcpus_used'],
+                    hypervisor_hostname=stat['hypervisor_hostname'],
+                    current_workload=stat['current_workload'])
 
     def hypervisors(self, tenant_id, compute_endpoint, token, tenant_name):
         host_stats = []
-        hypervisors = self._hypervisor_list(tenant_id, compute_endpoint, token, tenant_name)
+        hypervisors = self._hypervisor_list(tenant_id, compute_endpoint,
+                                            token, tenant_name)
         for h in hypervisors:
-            host_stats.append(self._hypervisor_detail(tenant_id, compute_endpoint, token, tenant_name, h))
+            host_stats.append(self._hypervisor_detail(tenant_id,
+                              compute_endpoint, token, tenant_name, h))
         return host_stats
 
     def take_action(self, parsed_args):
         keystone = client.Client(username=os.environ.get("OS_USERNAME"),
-            password=os.environ.get("OS_PASSWORD"),
-            tenant_name=os.environ.get("OS_TENANT_NAME"),
-            auth_url=os.environ.get("OS_AUTH_URL"))
+                                 password=os.environ.get("OS_PASSWORD"),
+                                 tenant_name=os.environ.get("OS_TENANT_NAME"),
+                                 auth_url=os.environ.get("OS_AUTH_URL"))
         for serv in keystone.service_catalog.catalog["serviceCatalog"]:
             if serv["type"] == 'compute':
                 self.compute_endpoint = serv["endpoints"][0]['adminURL']
@@ -105,11 +110,12 @@ class Compute(Command):
         self.token = keystone.auth_token
         self.tenant_id = keystone.tenant_id
         self.tenant_name = os.environ.get("OS_TENANT_NAME")
-        stats = self.hypervisors(self.tenant_id,self.compute_endpoint,self.token,self.tenant_name)
+        stats = self.hypervisors(self.tenant_id, self.compute_endpoint,
+                                 self.token, self.tenant_name)
         self.log.debug(stats)
-        if parsed_args.noheader == False:
+        if parsed_args.noheader is False:
             self._csv_header(stats[0].keys())
-        if parsed_args.noindividual == False:
+        if parsed_args.noindividual is False:
             self._individual_hypervisor_stats(stats)
-        if parsed_args.noaggregate == False:
+        if parsed_args.noaggregate is False:
             self._aggregate_hypervisor_stats(stats)

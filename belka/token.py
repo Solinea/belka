d700de9df6c2aa4c988ed2a2c3fa1d1a8ccec244
Ken Pepple <ken@pepple.info>
2013-03-30 17:19:16 -0700

    first tests for token

--- belka/token.py
+++ belka/token.py
@@ -1,4 +1,5 @@
 import ConfigParser
+import exception
 import logging
 import os
 
@@ -7,12 +8,15 @@ from keystoneclient.v2_0 import client
 log = logging.getLogger(__name__)
 
 
-def _headers(self, token, tenant_id):
-    return {"X-Auth-Token": token, "X-Auth-Project-Id": tenant_id,
-            "User-Agent": "belka", "Accept": "application/json"}
+def headers(token, tenant_id):
+    if token is None:
+        raise exception.MissingTokenError()
+    else:
+        return {"X-Auth-Token": token, "X-Auth-Project-Id": tenant_id,
+                "User-Agent": "belka", "Accept": "application/json"}
 
 
-def _get_credentials(self, config_file):
+def get_credentials(config_file):
     if config_file is None:
         username = os.environ.get("OS_USERNAME")
         password = os.environ.get("OS_PASSWORD")
@@ -29,22 +33,22 @@ def _get_credentials(self, config_file):
                 tenant_name=tenant_name, auth_url=auth_url)
 
 
-def get_token(self, config_file):
-    creds = self._get_credentials(config_file)
-    self.log.debug(creds)
+def get_token(config_file):
+    creds = get_credentials(config_file)
+    log.debug(creds)
     keystone = client.Client(username=creds["username"],
                              password=creds["password"],
                              tenant_name=creds["tenant_name"],
                              auth_url=creds["auth_url"])
     for serv in keystone.service_catalog.catalog["serviceCatalog"]:
         if serv["type"] == 'compute':
-            self.compute_endpoint = serv["endpoints"][0]['adminURL']
+            compute_endpoint = serv["endpoints"][0]['adminURL']
             break
     for serv in keystone.service_catalog.catalog["serviceCatalog"]:
         if serv["type"] == 'storage':
-            self.storage_endpoint = serv["endpoints"][0]['adminURL']
+            storage_endpoint = serv["endpoints"][0]['adminURL']
             break
     return dict(token=keystone.auth_token, tenant=keystone.tenant_id,
                 tenant_name=creds["tenant_name"],
-                compute_endpoint=self.compute_endpoint,
-                storage_endpoint=self.storage_endpoint)
+                compute_endpoint=compute_endpoint,
+                storage_endpoint=storage_endpoint)
--- tests/test_token.py
+++ tests/test_token.py
@@ -1,9 +1,31 @@
+import os
 import unittest
 
 import belka.token as token
+import belka.exception as exception
+
 
 class TestToken(unittest.TestCase):
 
-    def test_first(self):
-        pass
+    def test_happy_headers(self):
+        tenant = "1234"
+        auth_token = "4321"
+        happy_header = {'Accept': 'application/json',
+                        'User-Agent': 'belka',
+                        'X-Auth-Project-Id': '1234',
+                        'X-Auth-Token': '4321'}
+        self.assertEqual(happy_header, token.headers(auth_token, tenant))
 
+    def test_none_headers_raise_error(self):
+        self.assertRaises(exception.MissingTokenError,
+                          token.headers, None, None)
+
+    def test_environ_credentials(self):
+        os.environ.update(dict(OS_USERNAME="billy"))
+        os.environ.update(dict(OS_PASSWORD="12345"))
+        os.environ.update(dict(OS_TENANT_NAME="openstack"))
+        os.environ.update(dict(OS_AUTH_URL="http://example.com:5000/"))
+        happy_creds = dict(username="billy", password="12345",
+                           tenant_name="openstack",
+                           auth_url="http://example.com:5000/")
+        self.assertEqual(happy_creds, token.get_credentials(None))
